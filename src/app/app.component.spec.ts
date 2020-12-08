c74f67f0f6148fd23142519e61e0ecc671861090
Luis Pablo <wichos11.ch@gmail.com>
2020-12-07 23:04:51 -0600

    prueba 1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -27,4 +27,16 @@ describe('AppComponent', () => {
   });
 
  
+  it('should render title in a h1 tag', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Login');});
+
+    it('should render title in a h2 tag', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h2').textContent).toContain('Practica 1');});
+
 });
